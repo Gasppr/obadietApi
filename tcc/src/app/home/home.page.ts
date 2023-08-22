80fd0d83719c4aa1bd9b9926974c92b5ce21c38f
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-08-22 23:46:30 +0000

    FT003-HOME: chore - construção da estrutura base da página home

--- tcc/src/app/home/home.page.ts
+++ tcc/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
 
   constructor() {}
 
+  comidas = [
+    {
+      image: "assets/images/camaroes-deliciosos.png"
+    },
+    {
+      image: "assets/images/camaroes-deliciosos.png"
+    },
+    {
+      image: "assets/images/camaroes-deliciosos.png"
+    },
+    {
+      image: "assets/images/camaroes-deliciosos.png"
+    }
+  ]
+
+  swiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  swiperSlideChanged(e: any){
+    console.log('changed ', e);
+  }
 }
