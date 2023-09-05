d88851262c6098fd71961548f79d56ce8145cb70
Paulo Roberto Antunes De Carvalho <paulornatunes57@gmail.com>
2023-09-05 11:24:53 +0000

    API - Adicionando as variaveis de ambiente e configurando ao projeto

--- api-oba-diet/src/app.module.ts
+++ api-oba-diet/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
-
 import { UsuarioController } from './usuario/usuario.controller';
 import { ReceitasController } from './receitas/receitas.controller';
 import { UsuarioRepository } from './usuario/repository/Usuario.repository';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'src/config/configuration';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({load: [configuration]})],
   controllers: [UsuarioController, ReceitasController],
   providers: [UsuarioRepository],
 })
---
+++ api-oba-diet/src/config/configuration.ts
@@ -0,0 +1,16 @@
+
+
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 3000,
+    database: {
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+      user: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD,
+      url: process.env.DATABASE_URL 
+
+
+    }
+  });
+
+
