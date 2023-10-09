260c6d8e88cdb6fda43a9617d86dd0bb238a26b5
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-10-09 12:02:45 +0000

    FT003-HORARIOS: criação básica dos componentes de adicionar horários

---
+++ tcc/src/app/adicionar-horarios/horario-refeicao/horario-refeicao.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horario-refeicao',
+  templateUrl: './horario-refeicao.component.html',
+  styleUrls: ['./horario-refeicao.component.scss'],
+})
+export class HorarioRefeicaoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ tcc/src/app/adicionar-horarios/horario-remedio/horario-remedio.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horario-remedio',
+  templateUrl: './horario-remedio.component.html',
+  styleUrls: ['./horario-remedio.component.scss'],
+})
+export class HorarioRemedioComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
