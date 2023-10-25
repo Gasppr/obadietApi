5e18495759fcb7e356fecb5c8cbc2b0bd33f0744
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-10-25 00:34:33 +0000

    FT003-HOORARIOS: chore - criação básica da estrutura dos componentes de criação de horário e implementação da abertura dos mesmos

--- tcc/src/app/adicionar-horarios/horario-refeicao/horario-refeicao.component.ts
+++ tcc/src/app/adicionar-horarios/horario-refeicao/horario-refeicao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-horario-refeicao',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorarioRefeicaoComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
 
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss('confirm');
+  }
 }
--- tcc/src/app/adicionar-horarios/horario-remedio/horario-remedio.component.ts
+++ tcc/src/app/adicionar-horarios/horario-remedio/horario-remedio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-horario-remedio',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorarioRemedioComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
 
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss('confirm');
+  }
 }
