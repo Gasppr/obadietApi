504fb9ba03e9faad5da148a32014278585fdacf8
Luis Felippe Lima Moraes <luisfelippelimalima@gmail.com>
2023-11-14 11:34:17 +0000

    dev: Criação da estrutura da service de receitas.

---
+++ tcc/src/app/services/recipes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import{HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  private readonly URL = "https://obadietapi.vercel.app/obadiet/";
+
+  constructor(private http : HttpClient) { }
+
+  buscarReceitas():Observable<any>{
+    return  this.http.get<any>(`${this.URL}/receitas`)
+  }
+  }
