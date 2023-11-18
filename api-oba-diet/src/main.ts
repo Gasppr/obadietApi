5664fc85539482aa3f04ef8c503d259b4b27b22a
Paulo Roberto Antunes De Carvalho <paulornatunes57@gmail.com>
2023-11-18 17:52:28 +0000

    API-PROD : Ativando cors da API

--- api-oba-diet/src/main.ts
+++ api-oba-diet/src/main.ts
@@ -7,6 +7,7 @@ import { SwaggerConfig } from './config/SwaggerConfig';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors()
   app.useGlobalPipes
   (
     new ValidationPipe
