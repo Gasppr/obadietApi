781c56f0f1806262903413836f750c5c1af882aa
Paulo Roberto Antunes De Carvalho <paulornatunes57@gmail.com>
2023-11-18 19:03:05 +0000

    API-PROD: Adicionando dominios que podem acessar a API

--- api-oba-diet/src/main.ts
+++ api-oba-diet/src/main.ts
@@ -7,7 +7,16 @@ import { SwaggerConfig } from './config/SwaggerConfig';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  app.enableCors(
+    {origin: [
+      'http://localhost:8100',
+      'http://localhost:4200',
+      'http://obadiet.vercel.app',
+     
+    ],
+    methods:["OPTIONS", "GET", "POST","PUT", "PATCH", "DELETE"],
+    credentials: true,}
+  )
   app.useGlobalPipes
   (
     new ValidationPipe
