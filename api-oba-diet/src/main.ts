a15565471ae1f1cd558f960e63ce1f3fa7c3551e
Paulo Roberto Antunes De Carvalho <paulornatunes57@gmail.com>
2023-11-18 19:51:59 +0000

    deploy commit

--- api-oba-diet/src/main.ts
+++ api-oba-diet/src/main.ts
@@ -8,14 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(
-    {origin: [
-      'http://localhost:8100',
-      'http://localhost:4200',
-      'http://obadiet.vercel.app',
-     
-    ],
+    {
+    origin: true,
     methods:["OPTIONS", "GET", "POST","PUT", "PATCH", "DELETE"],
-    credentials: true,}
+    allowedHeaders:['Content-type' , 'Authorization']}
   )
   app.useGlobalPipes
   (
