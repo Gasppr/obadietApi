b3a92ffbb57227e298c409f5432a0c0669e78c54
Luis Felippe Lima Moraes <luisfelippelimalima@gmail.com>
2023-11-22 12:54:15 +0000

    FT005-RECEITAS: feat - Transferindo dados das receitas de uma pagina para outra utilizando o ID e geraçlão da página de detalhes da receita.

--- tcc/src/app/receitas/receitas.page.ts
+++ tcc/src/app/receitas/receitas.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RecipesService } from '../services/recipes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-receitas',
@@ -11,10 +12,13 @@ export class ReceitasPage implements OnInit {
 
   receitas$!: Observable<any>;
 
-  constructor(private recipesService: RecipesService) { }
+  constructor(private recipesService: RecipesService, private router: Router) { }
 
   ngOnInit() {
     this.receitas$ = this.recipesService.buscarReceitas();
   }
 
+  verDetalhes(receitaId: number) {
+    this.router.navigate(['obaDiet/receita'], { queryParams: { id: receitaId } });
+  }
 }
