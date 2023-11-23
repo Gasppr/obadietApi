371720638946b2ebffc8b64337ad587d75b21620
Luis Felippe Lima Moraes <luisfelippelimalima@gmail.com>
2023-11-23 21:21:01 +0000

    FT006-SEARCHBAR: feat - Adicionando funcionalidade a searchbar na pagina de receitas para poder buscar uma receita especifica.

--- tcc/src/app/receitas/receitas.page.ts
+++ tcc/src/app/receitas/receitas.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RecipesService } from '../services/recipes.service';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-receitas',
@@ -9,16 +10,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./receitas.page.scss'],
 })
 export class ReceitasPage implements OnInit {
+  receitas$!: Observable<any[]>;
+  filteredReceitas$!: Observable<any[]>;
+  searchTerm: string = '';
 
-  receitas$!: Observable<any>;
-
-  constructor(private recipesService: RecipesService, private router: Router) { }
+  constructor(private recipesService: RecipesService, private router: Router) {}
 
   ngOnInit() {
     this.receitas$ = this.recipesService.buscarReceitas();
+    this.filteredReceitas$ = this.receitas$;
   }
 
   verDetalhes(receitaId: number) {
     this.router.navigate(['obaDiet/receita'], { queryParams: { id: receitaId } });
+  }
+
+  onSearchChange(event: any) {
+    const searchTerm = event.detail.value.toLowerCase();
+
+    if (searchTerm.trim() !== '') {
+      this.filteredReceitas$ = this.receitas$.pipe(
+        map((receitas: any[]) =>
+          receitas.filter((receita: any) =>
+            receita.nome.toLowerCase().includes(searchTerm)
+          )
+        )
+      );
+    } else {
+      this.filteredReceitas$ = this.receitas$;
+    }
   }
 }
