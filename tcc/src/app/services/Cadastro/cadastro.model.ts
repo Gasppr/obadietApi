83bba361782b4452045936e6acacfd2a48376c47
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-11-24 04:59:54 +0000

    FT008-CADASTRO: criação das services para cadastro de usuário

---
+++ tcc/src/app/services/Cadastro/cadastro.model.ts
@@ -0,0 +1,10 @@
+export interface Cadastro{
+    id: string,
+    nome: string
+    email: string,
+    sexo: string,
+    idade: number,
+    peso: number,
+    altura: number,
+    senha: string
+}
