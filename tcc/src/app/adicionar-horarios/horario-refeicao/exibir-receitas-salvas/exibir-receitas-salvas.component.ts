8604b1d0617c6e404820416ae4378868b70dd979
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-11-26 23:37:45 +0000

    FT009-HORARIOS: chore - adição base de ion-segment para filtrar adição de receitas salvas ou por pesquisa

--- tcc/src/app/adicionar-horarios/horario-refeicao/exibir-receitas-salvas/exibir-receitas-salvas.component.ts
+++ tcc/src/app/adicionar-horarios/horario-refeicao/exibir-receitas-salvas/exibir-receitas-salvas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Observable, map } from 'rxjs';
+import { RecipesService } from 'src/app/services/recipes.service';
 
 interface ReceitaSelecionada{
   idRecSelec: number;
@@ -15,6 +18,12 @@ interface ReceitaSelecionada{
 
 export class ExibirReceitasSalvasComponent  implements OnInit {
   receitaSelecionada: ReceitaSelecionada;
+
+  receitas$!: Observable<any[]>;
+  filteredReceitas$!: Observable<any[]>;
+  searchTerm: string = '';
+  
+  selecionarCategoria: string = 'salvas'
 
   iniciarReceitaSelecionada(): ReceitaSelecionada {
     return { idRecSelec: 0, nomeRecSelec: '', imgRecSelec: ''}
@@ -43,13 +52,35 @@ export class ExibirReceitasSalvasComponent  implements OnInit {
     }
   ]
 
-  constructor(private modalCtrl: ModalController) {
+  constructor(private modalCtrl: ModalController, private recipesService: RecipesService) {
     this.receitaSelecionada = this.iniciarReceitaSelecionada();
   }
 
+  ngOnInit() {
+    this.receitas$ = this.recipesService.buscarReceitas();
+    this.filteredReceitas$ = this.receitas$;
+  }
 
+  mudarSelecionarCategoria(e: any){
+    this.selecionarCategoria = e.detail.value;
+    console.log(this.selecionarCategoria)
+  }
 
-  ngOnInit() {}
+  onSearchChange(event: any) {
+    const searchTerm = event.detail.value.toLowerCase();
+
+    if (searchTerm.trim() !== '') {
+      this.filteredReceitas$ = this.receitas$.pipe(
+        map((receitas: any[]) =>
+          receitas.filter((receita: any) =>
+            receita.nome.toLowerCase().includes(searchTerm)
+          )
+        )
+      );
+    } else {
+      this.filteredReceitas$ = this.receitas$;
+    }
+  }
 
   cancel() {
     return this.modalCtrl.dismiss(null, 'cancel');
