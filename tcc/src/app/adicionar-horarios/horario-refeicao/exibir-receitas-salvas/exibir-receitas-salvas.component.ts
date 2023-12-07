c1f7f61d3b3f80566793758ca07f54ce4822f01b
Mariana Ayumi Konno <marianakonno@gmail.com>
2023-12-07 00:11:34 -0300

    FT011-SALVOS: feat - carregamento de receitas salvas para seleção na criação de refeição

--- tcc/src/app/adicionar-horarios/horario-refeicao/exibir-receitas-salvas/exibir-receitas-salvas.component.ts
+++ tcc/src/app/adicionar-horarios/horario-refeicao/exibir-receitas-salvas/exibir-receitas-salvas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { Observable, map } from 'rxjs';
+import { StorageService } from 'src/app/services/Login/storage.service';
 import { RecipesService } from 'src/app/services/recipes.service';
 
 interface ReceitaSelecionada{
@@ -22,38 +23,18 @@ export class ExibirReceitasSalvasComponent  implements OnInit {
   receitas$!: Observable<any[]>;
   filteredReceitas$!: Observable<any[]>;
   searchTerm: string = '';
-  
+
   selecionarCategoria: string = 'salvas'
 
   iniciarReceitaSelecionada(): ReceitaSelecionada {
     return { idRecSelec: 0, nomeRecSelec: '', imgRecSelec: ''}
   }
 
-  receitasSalvas = [
-    {
-      idReceita: 1,
-      nome: 'camarao',
-      img: 'https://kipeixe.com.br/media/catalog/product/cache/1/thumbnail/600x/17f82f742ffe127f42dca9de82fb58b1/i/m/imagem_1_15_5.jpg'
-    },
-    {
-      idReceita: 2,
-      nome: 'camarao abacaxi',
-      img: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/194/977/products/abacaxi-perola11-d39b14434678c43cc815897563419666-640-0.jpg'
-    },
-    {
-      idReceita: 3,
-      nome: 'camarao batata',
-      img: 'https://scfoods.fbitsstatic.net/img/p/batata-lavada-500g-70629/257131.jpg?w=800&h=800&v=no-change&qs=ignore'
-    },
-    {
-      idReceita: 4,
-      nome: 'camarao cerveja',
-      img: 'https://i.ytimg.com/vi/NEENrXgoqcM/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AHmA4AC6AKKAgwIABABGHIgRygqMA8=&rs=AOn4CLAGybaQlMl_7KEACtSNLaPFFRSe9A'
-    }
-  ]
+  receitasSalvas: any;
 
-  constructor(private modalCtrl: ModalController, private recipesService: RecipesService) {
+  constructor(private modalCtrl: ModalController, private recipesService: RecipesService, private storage: StorageService) {
     this.receitaSelecionada = this.iniciarReceitaSelecionada();
+    this.buscarReceitasSalvas();
   }
 
   ngOnInit() {
@@ -79,6 +60,16 @@ export class ExibirReceitasSalvasComponent  implements OnInit {
     } else {
       this.filteredReceitas$ = this.receitas$;
     }
+  }
+
+  async buscarReceitasSalvas() {
+    let token = await this.storage.buscarToken("token");
+    await this.recipesService.buscarReceitasSalvas(token).subscribe({
+      next: (data: any) => {
+        this.receitasSalvas = data.receitasSalvas;
+        console.log(this.receitasSalvas)
+      }
+    })
   }
 
   cancel() {
